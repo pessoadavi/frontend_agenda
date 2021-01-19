1cde28ea0c7d621144c45a361e0fcae943aa7901
Davi Pessoa de Melo <pessoadavi@gmail.com>
2021-01-19 16:55:00 -0300

    Primeiro commit

---
+++ src/main/App.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const App = () => {
+    return ( 
+        <div className="container">
+            <h2>teste</h2>
+        </div>
+     );
+}
+ 
+export default App;
